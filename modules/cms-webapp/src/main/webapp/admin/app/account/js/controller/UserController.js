a3a4ee3e0e610120b1db7ebb7e25542216452174
Pavel Milkevich <pmi@enonic.com>
2011-09-27 17:14:15 +0300

    B-02279: Admin 2.0: Accounts: User Wizard: create navigator panel + Userstores and toolbar from Victor integrated

--- modules/cms-webapp/src/main/webapp/admin/app/account/js/controller/UserController.js
+++ modules/cms-webapp/src/main/webapp/admin/app/account/js/controller/UserController.js
@@ -24,7 +24,7 @@ Ext.define( 'App.controller.UserController', {
         'AddressContainer',
         'GroupDetailButton',
         'UserWizardToolbar',
-        'UserWizard',
+        'UserWizardPanel',
         'UserStoreListPanel'
     ],
 
@@ -291,9 +291,10 @@ Ext.define( 'App.controller.UserController', {
                 iconCls: 'icon-user-add',
                 closable: true,
                 autoScroll: true,
+                layout: 'fit',
                 items: [
                     {
-                        xtype: 'userWizard'
+                        xtype: 'userWizardPanel'
                     }
                 ]
             };
