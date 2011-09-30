fb1102371484db850566aa47355560d06674612b
Thomas Andersen <tan@enonic.com>
2011-09-30 19:32:01 +0200

    JsonSerializer, initial commit

--- modules/cms-core/src/main/java/com/enonic/cms/portal/xtrace/JsonSerializer.java
+++ modules/cms-core/src/main/java/com/enonic/cms/portal/xtrace/JsonSerializer.java
@@ -1,11 +1,148 @@
 package com.enonic.cms.portal.xtrace;
 
+import com.enonic.cms.portal.livetrace.DatasourceExecutionTrace;
 import com.enonic.cms.portal.livetrace.PageRenderingTrace;
+import com.enonic.cms.portal.livetrace.WindowRenderingTrace;
+import com.google.gson.*;
 
+import java.util.List;
+
+// TODO: Calculate the start times and end times
 public class JsonSerializer
 {
     public String serialize( PageRenderingTrace pageRenderingTrace )
     {
-        return "{  \"xtrace\": {    \"version\": 1,    \"page\": {      \"key\": 73,      \"name\": \"Home\",      \"cacheable\": false,      \"cache_hit\": false,      \"page_template_name\": \"document\",      \"start_time\": 0,      \"end_time\": 1692,      \"xslt\": {        \"processing_time\": 28      },      \"total_time\": 1692,      \"run_as_user\": \"local\\\\tan\",      \"functions\": [        {          \"name\": \"getMenuBranch\",          \"start_time\": 0,          \"end_time\": 1,          \"time\": 128        }      ],      \"windows\": [        {          \"key\": 1925,          \"name\": \"Frontpage banner (950px)\",          \"cacheable\": true,          \"cache_hit\": true,          \"start_time\": 128,          \"end_time\": 480,          \"total_time\": 352,          \"xslt\": {            \"processing_time\": 7          },          \"functions\": [            {              \"name\": \"getContentBySection\",              \"start_time\": 128,              \"end_time\": 1,              \"time\": 345            }          ]        },        {          \"key\": 1926,          \"name\": \"Case studies (frontpage box)\",          \"cacheable\": true,          \"cache_hit\": true,          \"start_time\": 480,          \"end_time\": 1368,          \"total_time\": 888,          \"xslt\": {            \"processing_time\": 19          },          \"functions\": [            {              \"name\": \"getContentBySection\",              \"start_time\": 480,              \"end_time\": 1,              \"time\": 39            },            {              \"name\": \"getContentBySection\",              \"start_time\": 519,              \"end_time\": 1,              \"time\": 528            },            {              \"name\": \"getMenuItem\",              \"start_time\": 1047,              \"end_time\": 1,              \"time\": 3            },            {              \"name\": \"getContentBySection\",              \"start_time\": 1050,              \"end_time\": 1,              \"time\": 295            },            {              \"name\": \"getMenuItem\",              \"start_time\": 1345,              \"end_time\": 1,              \"time\": 4            }          ]        },        {          \"key\": 1926,          \"name\": \"Vignette list (boxes)\",          \"cacheable\": true,          \"cache_hit\": true,          \"start_time\": 1368,          \"end_time\": 1664,          \"total_time\": 296,          \"xslt\": {            \"processing_time\": 19          },          \"functions\": [            {              \"name\": \"getContentBySection\",              \"start_time\": 1368,              \"end_time\": 1,              \"time\": 287            }          ]        }      ]    }  }}";
+        JsonObject wrapper = new JsonObject();
+
+        JsonObject xtrace = new JsonObject();
+        appendVersion( xtrace );
+        appendPage( xtrace, pageRenderingTrace );
+
+        wrapper.add( "xtrace", xtrace );
+
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        gsonBuilder.setPrettyPrinting();
+
+        Gson gson = gsonBuilder.create();
+
+        return gson.toJson( wrapper );
     }
+
+    private void appendVersion( JsonObject xtrace )
+    {
+        xtrace.addProperty( "version", "1.0" );
+    }
+
+    private void appendPage( JsonObject xtrace, PageRenderingTrace pageRenderingTrace )
+    {
+        xtrace.add( "page", createPage( pageRenderingTrace ) );
+    }
+
+    private JsonObject createPage( PageRenderingTrace pageRenderingTrace )
+    {
+        JsonObject page = new JsonObject();
+
+        page.addProperty( "key", "TODO" );
+        page.addProperty( "name", pageRenderingTrace.getPortalRequestTrace().getUrl() );
+        page.addProperty( "cacheable", "TODO" );
+        page.addProperty( "cache_hit", pageRenderingTrace.isUsedCachedResult() );
+        page.addProperty( "page_template_name", "TODO" );
+        page.addProperty( "start_time", 0 );
+        page.addProperty( "end_time", pageRenderingTrace.getDuration().getExecutionTimeInMilliseconds() );
+
+        JsonObject xsltObj = new JsonObject();
+        xsltObj.addProperty( "processing_time", pageRenderingTrace.getInstructionPostProcessingTrace().getDuration().getExecutionTimeInMilliseconds() );
+        page.add( "xslt", xsltObj );
+
+        page.addProperty( "total_time", pageRenderingTrace.getDuration().getExecutionTimeInMilliseconds() );
+
+        String userStoreName = "";
+        if ( pageRenderingTrace.getRenderer().getUserStoreName() != null )
+        {
+            userStoreName = pageRenderingTrace.getRenderer().getUserStoreName();
+        }
+
+        String qualifiedName = userStoreName + "\\" + pageRenderingTrace.getRenderer().getUsername();
+
+        page.addProperty( "run_as_user", qualifiedName );
+
+        appendPageDatasources( page, pageRenderingTrace );
+        appendWindows( page, pageRenderingTrace );
+
+        return page;
+    }
+
+    private void appendPageDatasources( JsonObject page, PageRenderingTrace pageRenderingTrace )
+    {
+        page.add( "datasources", createDatasources( pageRenderingTrace.getDatasourceExecutionTraces() ) );
+    }
+
+    private void appendWindows( JsonObject page, PageRenderingTrace pageRenderingTrace )
+    {
+        page.add( "windows", createWindows( pageRenderingTrace.getWindowRenderingTraces() ) );
+    }
+
+    private JsonArray createWindows( List<WindowRenderingTrace> windows )
+    {
+        JsonArray windowsArray = new JsonArray();
+
+        for ( WindowRenderingTrace window : windows )
+        {
+            createWindow( windowsArray, window );
+        }
+
+        return windowsArray;
+    }
+
+    private void createWindow( JsonArray windowsArray, WindowRenderingTrace window )
+    {
+        JsonObject windowObject = new JsonObject();
+        windowObject.addProperty( "key", "TODO" );
+        windowObject.addProperty( "name", window.getPortletName() );
+        windowObject.addProperty( "cacheable", "TODO" );
+        windowObject.addProperty( "cache_hit", window.isUsedCachedResult() );
+        windowObject.addProperty( "start_time", window.getDuration().getStartTime().toString() );
+        windowObject.addProperty( "end_time", window.getDuration().getStopTime().toString() );
+        windowObject.addProperty( "total_time", window.getDuration().getExecutionTimeInMilliseconds() );
+
+        JsonObject xsltObject = new JsonObject();
+        xsltObject.addProperty( "processing_time", window.getInstructionPostProcessingTrace().getDuration().getExecutionTimeInMilliseconds() );
+        windowObject.add( "xslt", xsltObject );
+
+        windowObject.addProperty( "run_as_user", window.getRenderer().getUsername() );
+
+        appendWindowDatasources( windowObject, window );
+
+        windowsArray.add( windowObject );
+    }
+
+    private void appendWindowDatasources( JsonObject windowObject, WindowRenderingTrace window )
+    {
+        windowObject.add( "datasources", createDatasources( window.getDatasourceExecutionTraces() ) );
+    }
+
+
+    private JsonArray createDatasources( List<DatasourceExecutionTrace> datasources )
+    {
+        JsonArray datasourceArray = new JsonArray();
+
+        for ( DatasourceExecutionTrace datasource : datasources )
+        {
+            createDatasource( datasourceArray, datasource );
+        }
+
+        return datasourceArray;
+    }
+
+    private void createDatasource( JsonArray jsonArray, DatasourceExecutionTrace datasource )
+    {
+        JsonObject datasourceObject = new JsonObject();
+        datasourceObject.addProperty( "name", datasource.getMethodName() );
+        datasourceObject.addProperty( "start_time", datasource.getDuration().getStartTime().toString() );
+        datasourceObject.addProperty( "end_time", datasource.getDuration().getStopTime().toString() );
+        datasourceObject.addProperty( "time", datasource.getDuration().getExecutionTimeInMilliseconds() );
+
+        jsonArray.add( datasourceObject );
+    }
+
 }
