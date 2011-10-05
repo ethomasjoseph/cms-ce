83abc7c655924996583fb2da80d353ee99f9847e
Pavel Milkevich <pmi@enonic.com>
2011-10-05 11:39:36 +0300

    B-02323: Admin 2.0: Refactoring: Accounts: Rename occurrences of "User" to "Account"

--- modules/cms-webapp/src/main/webapp/admin/app/account/js/view/GridPanel.js
+++ modules/cms-webapp/src/main/webapp/admin/app/account/js/view/GridPanel.js
@@ -1,6 +1,6 @@
 Ext.define( 'App.view.GridPanel', {
     extend: 'Ext.grid.Panel',
-    alias : 'widget.userGrid',
+    alias : 'widget.accountGrid',
 
     requires: ['Common.PersistentGridSelectionPlugin'],
     plugins: ['persistentGridSelection'],
