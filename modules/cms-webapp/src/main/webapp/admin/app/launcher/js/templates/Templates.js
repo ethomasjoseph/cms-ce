d894633f9796c1f3b19caf1348d74ca15a61d3cc
Thomas Andersen <tan@enonic.com>
2011-10-14 14:39:52 +0200

    B-02376: Admin 2.0: Activity Stream: Implement design: Message boxes html

--- modules/cms-webapp/src/main/webapp/admin/app/launcher/js/templates/Templates.js
+++ modules/cms-webapp/src/main/webapp/admin/app/launcher/js/templates/Templates.js
@@ -17,5 +17,38 @@ Templates.launcher = {
             '<p>Change Password</p>' +
             '<p>&nbsp;</p>' +
             '<p class="cms-logged-in-user-popup-log-out"><a href="index.html">Log Out</a></p>' +
+        '</div>',
+
+    activityStreamItem:
+        '<div class="cms-activity-stream-message">' +
+            '<table border="0" cellspacing="0" cellpadding="0">' +
+                '<tr>' +
+                    '<td valign="top" class="photo-container">' +
+                        '<img class="photo" src="{photo}"/>' +
+                    '</td>' +
+                    '<td valign="top">' +
+                        '<div class="display-name-location"><a href="javascript:;">{displayName}</a> via {location}</div>' +
+                        '<div>{action}: <a href="javascript:;">{description}</a></div>' +
+                    '</td>' +
+                '</tr>' +
+            '</table>' +
+            '<div class="date-links clearfix" style="clear:both">' +
+                '<table border="0" cellspacing="0" cellpadding="0">' +
+                    '<tr>' +
+                        '<td>' +
+                            '<span class="pretty-date">{prettyDate}</span>' +
+                        '</td>' +
+                        '<td>' +
+                            '<a href="javascript:;" class="favorite" style="visibility:hidden">Favorite</a>' +
+                        '</td>' +
+                        '<td>' +
+                            '<a href="javascript:;" class="comment" style="visibility:hidden">Comment</a>' +
+                        '</td>' +
+                        '<td>' +
+                            '<a href="javascript:;" class="more" style="visibility:hidden"><!-- --></a>' +
+                        '</td>' +
+                    '</tr>' +
+                '</table>' +
+           '</div>' +
         '</div>'
 };
