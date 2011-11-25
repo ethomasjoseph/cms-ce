3e7d917c488740d4da2783cf65356a80b35e2210
user <user@users-MacBook-Pro.local>
2011-11-25 09:44:07 +0100

    B-02436 Admin 2.0: Accounts: Applications Mega Menu

--- modules/cms-webapp/src/main/webapp/admin/dev/examples/MegaMenu.js
+++ modules/cms-webapp/src/main/webapp/admin/dev/examples/MegaMenu.js
@@ -1,70 +1,23 @@
 Ext.define( 'Common.MegaMenu', {
     extend: 'Ext.menu.Menu',
     alias: 'megaMenu',
-    width: 600,
-    height: 300,
     plain: true,
-    margin: '0 0 10 0',
+    padding: 10,
     bodyCls: 'cms-mega-menu',
-    floating: true,
-
-    items: new Ext.Component(
-        {
-            style: {
-                width: '100%'
-            },
-
-            // TODO: For testing purposes at the moment. We may use a DataView here.
-            tpl: Ext.create('Ext.XTemplate', '' +
-                    '<table>' +
-                    '<tr>' +
-                    '<th colspan="50">' +
-                    'Header' +
-                    '</th>' +
-                    '</tr>' +
-                    '<tr>' +
-                    '<td><a class="first-item" href="">Menu Item</a></td>' +
-                    '<td><a href="">Menu Item</a></td>' +
-                    '<td><a href="">Menu Item</a></td>' +
-                    '<td><a href="">Menu Item</a></td>' +
-                    '</tr>' +
-                    '<tr>' +
-                    '<th colspan="50">' +
-                    'Header' +
-                    '</th>' +
-                    '</tr>' +
-                    '<tr>' +
-                    '<td><a href="">Menu Item</a></td>' +
-                    '<td><a href="">Menu Item</a></td>' +
-                    '<td><a href="">Menu Item</a></td>' +
-                    '<td><a href="">Menu Item</a></td>' +
-                    '</tr>' +
-                    '<tr>' +
-                    '<th colspan="50">' +
-                    'Header' +
-                    '</th>' +
-                    '</tr>' +
-                    '<tr>' +
-                    '<td><a href="">Menu Item</a></td>' +
-                    '<td><a href="">Menu Item</a></td>' +
-                    '<td><a href="">Menu Item</a></td>' +
-                    '<td><a href="">Menu Item</a></td>' +
-                    '</tr>' +
-                    '</table>'
-
-            ),
-
-            initComponent: function() {
-                var me = this;
-
-                me.update({ });
-
-                Ext.EventManager.addListener(document, 'keydown', function(event, target, o) {
-
-                }, me);
-
-                me.callParent( arguments );
-            }
+    loader: {
+        url: 'mega-menu.data',
+        params: {
+            userId: 1
+        },
+        tpl: '{id} - {text}',
+        renderer: function(loader, response, active) {
+            alert( response );
+            var text = response.responseText;
+            loader.getTarget().update('The response is ' + text);
+            return true;
         }
-    )
+    }
+
+
+
 } );
