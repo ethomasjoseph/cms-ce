45cdb96ec411decc515ad394a958f5ae83bba454
Viktar Fiodarau <vfi@enonic.com>
2011-12-02 09:50:55 +0100

    Add CountryServiceImpl test

--- modules/cms-spock-test/src/test/groovy/com/enonic/cms/core/country/CountryServiceImplTest.groovy
+++ modules/cms-spock-test/src/test/groovy/com/enonic/cms/core/country/CountryServiceImplTest.groovy
@@ -10,9 +10,54 @@ class CountryServiceImplTest extends Specification
     @Autowired
     CountryServiceImpl countryService
 
-    def "spring context test"()
+    def "test get country"()
     {
-        expect:
+        setup:
+        def country
+
+        expect: "country service is not null"
         countryService != null
+
+        when: "trying to get nonexisting country"
+        country = countryService.getCountry new CountryCode("NO")
+        then:
+        country == null
+
+        when: "trying to get existing country"
+        country = countryService.getCountry new CountryCode("BB")
+        then:
+        country != null
+        country.code.toString() == "BB"
+        country.englishName == "BARBADOS"
+
+    }
+
+    def "test get countries"()
+    {
+        setup:
+        def countries
+        def iterator
+
+        when: "countries should be not null"
+        countries = countryService.getCountries()
+
+        then:
+        countries != null
+
+        when: "and there should be an iterator"
+        iterator = countries.iterator()
+        then:
+        iterator != null
+
+        expect:
+        countries.size() == 3
+        iterator.next().code.toString() == "BB"
+        iterator.next().code.toString() == "GY"
+        iterator.next().code.toString() == "UG"
+        iterator.hasNext() == false
+
+
+
+
     }
 }
