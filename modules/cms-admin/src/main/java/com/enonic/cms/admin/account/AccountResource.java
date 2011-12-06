9afa5cca8144203fcd2b8b9d47759e13f8e55692
Alex Rodriguez <aro@enonic.com>
2011-12-06 13:34:51 +0100

    B-02438: Admin 2.0: Accounts: Export Accounts (back-end) Rest handler for account/export supports parameter with list of account keys.

--- modules/cms-admin/src/main/java/com/enonic/cms/admin/account/AccountResource.java
+++ modules/cms-admin/src/main/java/com/enonic/cms/admin/account/AccountResource.java
@@ -23,10 +23,12 @@ import com.enonic.cms.core.search.FacetEntry;
 import com.enonic.cms.core.search.Facets;
 import com.enonic.cms.core.search.SearchSortOrder;
 import com.enonic.cms.core.search.account.AccountIndexField;
+import com.enonic.cms.core.search.account.AccountKey;
 import com.enonic.cms.core.search.account.AccountSearchHit;
 import com.enonic.cms.core.search.account.AccountSearchQuery;
 import com.enonic.cms.core.search.account.AccountSearchResults;
 import com.enonic.cms.core.search.account.AccountSearchService;
+import com.enonic.cms.core.search.account.AccountType;
 import com.enonic.cms.core.security.group.GroupEntity;
 import com.enonic.cms.core.security.group.GroupKey;
 import com.enonic.cms.core.security.user.UserEntity;
@@ -138,14 +140,30 @@ public final class AccountResource
 
     @POST
     @Path("export")
-    public Response exportAsCsv( @InjectParam final AccountLoadRequest req,
-                                 @DefaultValue("ISO-8859-1") @QueryParam("encoding") String characterEncoding )
+    public Response exportAsCsv( @InjectParam final AccountExportRequest req,
+                                 @DefaultValue("ISO-8859-1") @FormParam("encoding") String characterEncoding )
         throws UnsupportedEncodingException
     {
         final int accountsExportLimit = 5000;
-        req.setLimit( accountsExportLimit );
-        final AccountSearchResults searchResults = search( req );
 
+        final AccountSearchResults searchResults;
+        if ( req.getKeys().length > 0 )
+        {
+            searchResults = getAccountListForKeys( req.getKeys() );
+        }
+        else
+        {
+            final AccountLoadRequest searchRequest = new AccountLoadRequest();
+            searchRequest.setOrganizations( req.getOrganizations() );
+            searchRequest.setUserstores( req.getUserstores() );
+            searchRequest.setSelectGroups( req.isSelectGroups() );
+            searchRequest.setSelectUsers( req.isSelectUsers() );
+            searchRequest.setQuery( req.getQuery() );
+            searchRequest.setSort( req.getSort() );
+            searchRequest.setSortDir( req.getSortDir() );
+            searchRequest.setLimit( accountsExportLimit );
+            searchResults = search( searchRequest );
+        }
         final AccountsCsvExport csvExport = new AccountsCsvExport( groupDao, userDao );
         final String content = csvExport.generateCsv( searchResults );
         final String filename = csvExport.getExportFileName( new Date() );
@@ -153,8 +171,24 @@ public final class AccountResource
 
         final byte[] data = content.getBytes( characterEncoding );
 
-        return Response.ok( data ).type( "text/csv; charset=" + characterEncoding ).header( "Content-Encoding", characterEncoding ).header(
-            "Content-Disposition", attachmentHeader ).build();
+        return Response.ok( data )
+            .type( "text/csv; charset=" + characterEncoding )
+            .header( "Content-Encoding", characterEncoding )
+            .header( "Content-Disposition", attachmentHeader )
+            .build();
+    }
+
+    private AccountSearchResults getAccountListForKeys( final String[] keys )
+    {
+        // TODO: refactor this when accounts API and model classes are in place
+        final AccountSearchResults accounts = new AccountSearchResults( 0, keys.length );
+        for ( final String key : keys )
+        {
+            final AccountType type = userDao.findByKey( key ) == null ? AccountType.GROUP : AccountType.USER;
+            final AccountSearchHit account = new AccountSearchHit( new AccountKey( key ), type, 0 );
+            accounts.add( account );
+        }
+        return accounts;
     }
 
     @GET
