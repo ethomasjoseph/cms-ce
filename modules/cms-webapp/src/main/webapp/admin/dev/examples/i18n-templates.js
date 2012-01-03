e69830bf779921681d78cfd88223c3d9a92ecfdd
Thomas Andersen <tan@enonic.com>
2012-01-03 12:50:13 +0100

    i18n: Create initial lib for i18n messages - Refactoring

--- modules/cms-webapp/src/main/webapp/admin/dev/examples/i18n-templates.js
+++ modules/cms-webapp/src/main/webapp/admin/dev/examples/i18n-templates.js
@@ -2,7 +2,7 @@ var tpl = new Ext.XTemplate(
     '<div style="background-color:LemonChiffon;">',
     '<div>Inside a template</div>',
     '<div style="font-size: 120%">',
-    '{[App.lib.i18n.Phrases.getPhrase("content.quickBrownFox", "brune", "r¾v", "dovne", "hund")]}',
+    '{[App.lib.util.Messages.get("exampleText", "black", "honey badger", "sleeping", "tiger")]}',
     '</div>',
     '</div>'
 );
